0c7696fb1222b2c399f2515a935af19557aeb0ce
Krystian Kosior <krystiankosior1905@gmail.com>
2022-10-18 21:22:07 +0200

    zalozenie nowego projektu/konfiguracja apache2/ konfiguracja bazy danych/ konfiguracja plik .env

---
+++ app/Database/Migrations/2022-10-18-185622_Currency.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Currency extends Migration
+{
+public function up()
+	{
+		$this->forge->addField([
+			'id' => [
+			'type' => 'VARCHAR',
+                        'constraint'=>36,
+                'unique' => TRUE,
+                'default' => 'UUID()',
+		'comment' => 'ID '
+			],
+			'name' => [
+				'type' => 'VARCHAR',
+				'constraint' => 40,
+				'null' => false,
+				'comment' => 'nazwa waluty'
+			],
+			'currency_code' => [
+				'type' => 'VARCHAR',
+				'constraint' => 40,
+				'null' => false,
+				'comment' => 'skrót'
+			],
+			'exchange_rate' => [
+				'type' => 'decimal(10,4)',
+				'null' => false,
+				'comment' => 'wartosc'
+			]
+			
+                
+            
+			]);
+			$this->forge->addKey('id', true);
+			
+			$this->forge->createTable('Currency');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('Currency');
+	}
+}
